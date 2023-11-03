7f4e8d9b1859d310269d6344862d5287190bc29c
Zakariae Ait sliman <zait-sli@student.1337.ma>
2023-11-03 10:40:48 +0100

    update

--- script.sh
+++ script.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
 
+apt-get update
+
+apt-get install build-essential libncurses-dev bison flex libssl-dev libelf-dev make gcc wget
+
+wget https://bugs.launchpad.net/ubuntu/+archive/primary/+sourcefiles/linux/6.2.0-20.20/linux_6.2.0.orig.tar.gz
+
+tar -xvf linux_6.2.0.orig.tar.gz
+cd linux-6.2.0
+
+cp /boot/config-$(uname -r) .config
+
 mkdir -p /overlay/{upper,lower,work,merged}
 mount -t overlay -o lowerdir=/overlay/lower,upperdir=/overlay/upper,workdir=/overlay/work overlay /overlay/merged
+
+make -j$(nproc)
+make modules_install install
+update-grub
+reboot
